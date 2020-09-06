9be8cdf1b39a0f5b15444b5c75aa948c8322d41a
Pedro Soares <pedro.soares@systemhope.com.br>
2020-09-06 10:36:00 -0300

    project structure

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import * as bodyParser from 'body-parser'
+import routes from './routes'
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(routes)
+
+app.listen(3333)
